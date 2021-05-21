2a642c55ed3cadbadc658f6808e3a7de1790a9eb
Anatoliy Morozan <zoomytfunny@gmail.com>
2021-05-21 21:47:37 +0300

    Updated db

--- db.sql
+++ db.sql
@@ -1,6 +1,31 @@
-CREATE DATABASE `bot`;
-CREATE TABLE `bot`.`users` (
-`user_id` INT NOT NULL,
-`reg_date` DATETIME NOT NULL,
-PRIMARY KEY (`user_id`))
-COLLATE='utf8mb4_0900_ai_ci';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Дамп структуры базы данных vk_bot
+CREATE DATABASE IF NOT EXISTS `vk_bot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `vk_bot`;
+
+-- Дамп структуры для таблица vk_bot.chats
+CREATE TABLE IF NOT EXISTS `chats` (
+  `chat_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `messages` int NOT NULL,
+  `symbols` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Дамп структуры для таблица vk_bot.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL,
+  `reg_date` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
