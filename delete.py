6a88b2d2f69d298d8a097df662566ab1eabbb4ca
Anatoliy Morozan <zoomytfunny@gmail.com>
2021-05-21 16:56:11 +0300

    Added deletion of all unnecessary files (ie if you create a file example.json and run delete.py, then after you type "delete unimportant files" it will be deleted)

--- delete.py
+++ delete.py
@@ -12,5 +12,13 @@ while True:
         print("All logs was deleted\n")
         find = 1
 
+    if want == "delete unimportant files":
+        files = glob.glob(os.getcwd() + "/*.*")
+        for f in files:
+            if not f.endswith('users.json') and not f.endswith('delete.py') and not f.endswith('commands.py') and not f.endswith('config.py') and not f.endswith('main.py'):
+                os.remove(f)
+        print("All files was deleted\n")
+        find = 1
+
     if find == 0:
         print('\nCommand "' + str(want) + '" not found!\n')
